77102124ff3a193449897e13d8777663ed149155
Mario F. Flórez D <marioffdsw@gmail.com>
2024-03-15 18:44:35 -0500

    Passing test given 1 should return "1"

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,8 +1,8 @@
-import {sum} from '../src/index';
+import { calculateFizzBuzz } from '../src';
 
-describe("test that the test framework is in place", () => {
-  test('test', () => {
-    let result = sum(1, 2);
-    expect(result).toBe(3);
+describe("GIVEN calculateFizBuzz", () => {
+  test('GIVEN 1 SHOULD return "1"', () => {
+    const result: string = calculateFizzBuzz(1);
+    expect(result).toBe("1");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 
-export function sum(a: number, b: number): number {
-  return a + b;
+export function calculateFizzBuzz(num: number): string {
+  return "1";
 }
