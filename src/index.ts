e86a06b9b167cd6d922a185f4cd2f8cd9b250380
Mario F. Flórez D <marioffdsw@gmail.com>
2024-03-15 19:05:39 -0500

    Refactor case for multiples of 3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 
 export function calculateFizzBuzz(num: number): string {
-  if(num == 3) return "Fizz";
-  if(num == 5) return "Buzz";
   if(num == 15) return "FizzBuzz";
-
   if(num % 3 == 0) return "Fizz";
+  if(num == 5) return "Buzz";
+
 
   return num.toString();
 }
