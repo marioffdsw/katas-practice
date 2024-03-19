1973ab262a858f61221daf08cf51eef9e5173e0d
Mario F. Flórez D <marioffdsw@gmail.com>
2024-03-19 12:05:40 -0500

    Session 2

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,8 +1,75 @@
-import {sum} from '../src/index';
+import {Game} from '../src/index';
 
-describe("test that the test framework is in place", () => {
-  test('test', () => {
-    let result = sum(1, 2);
-    expect(result).toBe(3);
+describe("GIVEN a game", () => {
+  test('WHEN using constructor THEN should be created', () => {
+    const game  = new Game();
+    expect(game).not.toBeNull();
+  });
+
+  function gameWithoutBonus(game: Game, pins: number) {
+    for (let i = 0; i <= 19; i++) {
+      game.roll(pins);
+    }
+  }
+
+  test('WHEN all rolls are 0 THEN score should be 0', () => {
+    const game  = new Game();
+    gameWithoutBonus(game, 0);
+    const score = game.score;
+    expect(score).toBe(0);
+  });
+
+  test('WHEN rolling a spare AND rolling 3 in the next roll THEN score should be 16', () => {
+    const game  = new Game();
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    for(let i = 0; i <= 16; ++i) {
+      game.roll(0);
+    }
+    const score = game.score;
+    expect(score).toBe(16);
+  });
+
+  test('WHEN all rolls are 1 THEN score should be 20', () => {
+    const game  = new Game();
+    gameWithoutBonus(game, 1);
+    const score = game.score;
+    expect(score).toBe(20);
+  });
+
+  test('WHEN all rolls are 3 THEN score should be 60', () => {
+    const game  = new Game();
+    gameWithoutBonus(game, 3);
+    const score = game.score;
+    expect(score).toBe(60);
+  });
+
+  test('WHEN rolling a spare AND rolling 0 in the next roll THEN score should be 10', () => {
+    const game  = new Game();
+    game.roll(5);
+    game.roll(5);
+    for(let i = 0; i <= 17; ++i) {
+      game.roll(0);
+    }
+    const score = game.score;
+    expect(score).toBe(10);
+  });
+
+  test('WHEN rolling a spare AND rolling 0 in the next roll THEN score should be 10', () => {
+    const game  = new Game();
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    game.roll(0);
+    game.roll(5);
+    game.roll(5);
+    game.roll(3);
+    game.roll(0);
+    for(let i = 0; i <= 11; ++i) {
+      game.roll(0);
+    }
+    const score = game.score;
+    expect(score).toBe(32);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
 
-export function sum(a: number, b: number): number {
-  return a + b;
+export class Game {
+  rollCount = 0;
+  lastFrameScore = 0;
+  score: number = 0
+
+  roll(pins: number) {
+    this.score += pins;
+
+    if(this.rollCount == 2) {
+      this.rollCount = 0;
+      if (this.lastFrameScore == 10) {
+        this.score += pins;
+      }
+      this.lastFrameScore = 0;
+    } else {
+      this.lastFrameScore += pins;
+    }
+
+    this.rollCount++;
+  }
 }
